0df1cfc5ad0008facd65922012f7135e960fbc9f
Rebeka Geczi <geczirebeka@gmail.com>
2020-09-08 16:28:37 +0100

    folder controller setup

---
+++ src/main/java/com/codeclan/example/onetomany/controllers/FileController.java
@@ -0,0 +1,11 @@
+package com.codeclan.example.onetomany.controllers;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class FileController {
+
+
+}
---
+++ src/main/java/com/codeclan/example/onetomany/controllers/UserController.java
@@ -0,0 +1,4 @@
+package com.codeclan.example.onetomany.controllers;
+
+public class UserController {
+}
